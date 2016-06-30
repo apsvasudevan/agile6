b32a7e852cfb94ea24e01cc7b6b1526680049c2d
Brian Feeny <brianfeeny@fas.harvard.edu>
2016-06-30 18:28:43 -0400

    Basic admin unit test, view only, will continue to expand on it

--- coreprotocols/coreprotocols/settings.py
+++ coreprotocols/coreprotocols/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
