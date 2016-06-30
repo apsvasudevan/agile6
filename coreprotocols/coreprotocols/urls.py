78da715730bc692ce30a942894ac379c5d8ebe7b
Christian A. Schlubach <christian.schlubach@gmail.com>
2016-06-30 08:54:38 -0400

    added landing page and corresponding login/logout info in settings.

--- coreprotocols/coreprotocols/urls.py
+++ coreprotocols/coreprotocols/urls.py
@@ -26,10 +26,12 @@ Including another URLconf
 
 """
 from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^', include('django.contrib.auth.urls')),
+
+    url(r'^', include('teamspirit.urls')),
 ]
---
+++ coreprotocols/teamspirit/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.landing_page,  name='landing_page'),
+]
