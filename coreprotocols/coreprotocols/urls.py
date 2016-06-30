95ed78a4d01e4cf03b7c728a64b4f296999227ed
Christian A. Schlubach <christian.schlubach@gmail.com>
2016-06-30 05:58:46 -0400

    added a login page and deleted migrations and .pyc files from the repo

--- coreprotocols/coreprotocols/urls.py
+++ coreprotocols/coreprotocols/urls.py
@@ -12,10 +12,24 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
+    auth urls includes:
+
+    ^login/$ [name='login']
+    ^logout/$ [name='logout']
+    ^password_change/$ [name='password_change']
+    ^password_change/done/$ [name='password_change_done']
+    ^password_reset/$ [name='password_reset']
+    ^password_reset/done/$ [name='password_reset_done']
+    ^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$ [name='password_reset_confirm']
+    ^reset/done/$ [name='password_reset_complete']
+
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls')),
 ]
