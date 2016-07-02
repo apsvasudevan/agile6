cfa21e5472a9cf31d9cf1e63d7dc7faf6cd2a9d5
Vasu Swamidurai <apsvasudevan@gmail.com>
2016-07-01 23:11:03 -0400

    updating settings to include static_root

--- coreprotocols/coreprotocols/settings.py
+++ coreprotocols/coreprotocols/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/share/ngnix/html/static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
