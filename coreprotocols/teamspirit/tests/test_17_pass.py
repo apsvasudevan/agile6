74141be5d4718f19deb2cfd0a202ee2182c45cca
Vasu Swamidurai <apsvasudevan@gmail.com>
2016-07-06 23:13:47 -0400

    Adding additional tests

---
+++ coreprotocols/teamspirit/tests/test_17_pass.py
@@ -0,0 +1,52 @@
+# ./manage.py test --pattern="test_17_pass.py"
+from django.contrib.auth.models import User
+from django.test import TestCase
+from teamspirit.models import Team, Session, SessionState
+
+class Test17PassSession(TestCase):
+
+    def setUp(self):
+
+        foxy = User.objects.create(username='foxy', email='admin@mysite.com', password='meantime', first_name="Fox", last_name="Panks")
+        self.joan = User.objects.create(username='joan', email='admin@mysite.com', password='meantime', first_name="Joan", last_name="Torche")
+        ellen = User.objects.create(username='ellen', email='admin@mysite.com', password='meantime', first_name="Ellen", last_name="Nice")
+
+        # create team
+        team = Team()
+        team.owner=foxy
+        team.name="TestTeam"
+        team.save()
+        team.members = [foxy, self.joan, ellen]
+        team.save()
+
+        # create session
+        session = Session()
+        session.team = team
+        session.name = "StandUp Test"
+        session.is_open = True
+        session.save()
+
+    def test_session_name(self):
+        session = Session.objects.get(name="StandUp Test")
+        self.assertEquals(session.name, "StandUp Test")
+
+    def test_session_member_count(self):
+        session = Session.objects.get(name="StandUp Test")
+        self.assertEquals(session.team.name, "TestTeam")
+
+    def test_session_is_open(self):
+        session = Session.objects.get(name="StandUp Test")
+        self.assertEquals(session.is_open, True)
+
+    def test_pass(self):
+        ss = SessionState()
+        session = Session.objects.get(name="StandUp Test")
+        ss.user = self.joan
+        ss.session = session
+        ss.state = "Pass"
+        ss.save()
+        self.assertEquals(ss.state, "Pass")
+
+
+
+
